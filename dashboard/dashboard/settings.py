3112de29a1dcb35239289f199d0a535942b1464f
shinomiya <makoto.shinomiya@icloud.com>
2019-07-21 18:03:41 +0900

    Update ALLOWED_HOSTS

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=stpmw2pq0*w(_2t4t90#j_*b$%#7q0e3jk(we#ttyp#y(#$qe'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
